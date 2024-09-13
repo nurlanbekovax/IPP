250f8a96906be566a1e511ac4f638a8be38c1a57
Nurshat <130434219+nurlanbekovax@users.noreply.github.com>
2024-09-13 12:51:27 +0600

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,56 @@
+# a = 3
+# print(type(a))
+#
+# a = 3.5
+# print(type(a))
+#
+# a = float(3.5)
+# print(type(a))
+#
+#
+# a = float(3)
+# b = int(3.6)
+# print(a)
+# print(b)
+#
+# c = 'my name'
+# print(type(c))
+
+# isStudent = True
+# print(isStudent)
+#
+# isStudent = False
+# print(isStudent)
+# print(type(isStudent))
+
+# isStudent = 'not student'
+# print(isStudent)
+# print(type(isStudent))
+
+# isStudent = int(True)
+# print(isStudent)
+
+# isStudent = 5.1
+# isStudent = float(5.1)
+
+# name = "Mark"
+# name = str("Mark")
+# age = int(18)
+# # isStudent = True
+# print(f'{name} and {age}')
+# print(isStudent)
+
+# a = 34
+# b = 56
+# print(a)
+# print(b)
+
+# name = int(input("How old r u?"))
+# if name >= 18:
+#     print("u r an adult")
+# else:
+#     print("minor")
+# print(name)
+
+# name = input("How old r u?")
+# print(f'I am {name} old')
